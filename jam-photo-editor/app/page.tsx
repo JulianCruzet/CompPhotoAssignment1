fd35dfc8cd8b1f358f3c804514ee96e2c57d37fb
JJULIIXN <julian2004cruzet@gmail.com>
2024-12-02 09:45:14 -0500

    Revert "Merge branch 'main' of https://github.com/JulianCruzet/CompPhotoAssignment1"
    This reverts commit 6ca6f8ca8c3ab5513247362b9520e5658a1d8ffd, reversing
    changes made to 7a807874154bb65a4b98e21cdc812e8725186adb.

--- jam-photo-editor/app/page.tsx
+++ jam-photo-editor/app/page.tsx
@@ -2,7 +2,7 @@ import PhotoEditor from '../components/PhotoEditor'
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <PhotoEditor />
     </main>
   )
